da1e8479d9b404351cc8b260aa32b2b8c5b49c67
Karn Wong <karn@karnwong.me>
2025-08-26 10:50:59 +0700

    add spaceship-titanic

---
+++ spaceship_titanic.py
@@ -0,0 +1,104 @@
+# https://www.kaggle.com/competitions/spaceship-titanic
+import pandas as pd
+import polars as pl
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+# read data
+df = pl.read_csv("data/train.csv")
+
+id_col = "PassengerId"
+y_col = "Transported"
+
+x_cols_numeric = ["Age", "RoomService", "FoodCourt", "ShoppingMall", "Spa", "VRDeck"]
+x_cols_categorial = ["HomePlanet", "CryoSleep", "Cabin", "Destination", "VIP"]
+x_cols = x_cols_numeric + x_cols_categorial
+
+# encode
+encs = {}
+df_encoded = pl.DataFrame()
+df_encoded = df_encoded.with_columns(df[id_col].alias(id_col))
+
+for i in x_cols_numeric:
+    df_encoded = df_encoded.with_columns(df[i].alias(i))
+
+for i in x_cols_categorial:
+    # encode
+    enc = OneHotEncoder(handle_unknown="ignore", sparse_output=False)
+    values = df[i].to_numpy().reshape(-1, 1)
+    enc.fit(values)
+    encs[i] = enc
+
+    # transform
+    one_hot_encoded_data = enc.fit_transform(values)
+    one_hot_df = pd.DataFrame(
+        one_hot_encoded_data, columns=enc.get_feature_names_out(["category"])
+    )
+    encoded_series = one_hot_df.idxmax(axis=1)
+
+    # map these column names to a simple numeric value
+    mapping = {name: i for i, name in enumerate(one_hot_df.columns)}
+    numeric_series = encoded_series.map(mapping)
+    df_encoded = df_encoded.with_columns(pl.Series(numeric_series.to_list()).alias(i))
+
+le = LabelEncoder()
+le.fit(df[y_col])
+y_encoded = le.transform(df[y_col])
+df_encoded = df_encoded.with_columns(pl.Series(y_encoded).alias(y_col))
+
+
+# train model
+rdf = RandomForestClassifier(
+    n_estimators=5000, max_depth=50, max_leaf_nodes=5, random_state=2
+)
+rdf.fit(df_encoded[x_cols], df_encoded[y_col])
+
+# prep test data
+df_test = pl.read_csv("data/test.csv")
+df_test_encoded = pl.DataFrame()
+
+df_test_encoded = df_test_encoded.with_columns(df_test[id_col].alias(id_col))
+
+for i in x_cols_numeric:
+    df_test_encoded = df_test_encoded.with_columns(df_test[i].alias(i))
+
+for i in x_cols_categorial:
+    values = df_test[i].to_numpy().reshape(-1, 1)
+
+    # transform
+    one_hot_encoded_data = encs[i].fit_transform(values)
+    one_hot_df = pd.DataFrame(
+        one_hot_encoded_data, columns=encs[i].get_feature_names_out(["category"])
+    )
+    encoded_series = one_hot_df.idxmax(axis=1)
+
+    # map these column names to a simple numeric value
+    mapping = {name: i for i, name in enumerate(one_hot_df.columns)}
+    numeric_series = encoded_series.map(mapping)
+    df_test_encoded = df_test_encoded.with_columns(
+        pl.Series(numeric_series.to_list()).alias(i)
+    )
+
+pred = rdf.predict(df_test_encoded[x_cols])
+pred = le.inverse_transform(pred)
+
+df_prediction = pl.DataFrame()
+df_prediction = df_prediction.with_columns(df_test[id_col].alias(id_col))
+df_prediction = df_prediction.with_columns(pl.Series(pred).alias(y_col))
+df_prediction.to_pandas().to_csv(
+    "data/prediction.csv", index=False
+)  # polars write true/false, but kaggle expects capitalized form
+
+
+# evaluate
+df_ground_truth = pl.read_csv("data/submission.csv").rename(
+    {y_col: f"{y_col}_ground_truth"}
+)
+
+df_prediction = df_prediction.join(
+    df_ground_truth, on=id_col, how="inner"
+).with_columns((pl.col(y_col) == pl.col(f"{y_col}_ground_truth")).alias("correct"))
+
+correct = len(df_prediction.filter(pl.col("correct")))
+accuracy = correct / len(df_prediction)
+print(f"accuracy: {accuracy}")
